a843d9d0802f49d03da7fc4f42687b03dc81873b
praditus343 <88468631+praditus343@users.noreply.github.com>
2023-11-04 01:43:43 +0700

    Update

---
+++ Biodata/Biodata.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "=========="; cout << endl;
+	cout << "hello word"; cout << endl;
+	cout << "=========="; cout << endl;
+	cout << endl;
+	
+	cout << "Nama 		: Praditus Egi Danuarta"; cout << endl;
+	cout << "NIM 		: 22.11.4662"; cout << endl;
+	cout << "Jurusan 	: Informatika"; cout << endl;
+	cout << "Asal 		: Landak,Kalbar"; cout << endl;
+}
